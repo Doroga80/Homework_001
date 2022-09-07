a763683d9f9948b53323e1e1fdebb6e7c258068e
RRN <nigercanis0@gmail.com>
2022-09-07 23:28:32 +0300

    Init

---
+++ test_№23/Program.cs
@@ -0,0 +1,21 @@
+﻿//Программа, которая принимает на вход число (N) и выдает таблицу кубов 
+//чисел от 1 до N.
+void CubeTable()
+{
+    Console.WriteLine("Введите число n:");
+    int n = Convert.ToInt32(Console.ReadLine());
+    for (int i=1; i<=n; i++)
+    {
+        Console.WriteLine($"{i*i*i}, ");
+    }
+}    
+
+try
+{
+    CubeTable();
+}
+catch
+{
+    Console.WriteLine("Необходимо вводить именно число");
+}
+
