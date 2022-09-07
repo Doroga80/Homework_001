fd7c81720effb92056dca05f2ea2ff55c8c9d266
RRN <nigercanis0@gmail.com>
2022-09-07 20:54:49 +0300

    Init

---
+++ test_№2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Программа, которая на вход принимает двачисла и выдает, каеое
+//число больше, а какое меньше.
+
+Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = Convert.ToInt32(Console.ReadLine());
+ 
+if(a>b)
+{
+    Console.WriteLine("max=" + a);
+}
+else if(a<b)
+{
+    Console.WriteLine("max=" + b);
+}
