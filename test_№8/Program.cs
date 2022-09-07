d2dc8d2940621030ceb3cc6d9dbc4de0335f96e4
RRN <nigercanis0@gmail.com>
2022-09-07 21:29:53 +0300

    Init

---
+++ test_№8/Program.cs
@@ -0,0 +1,13 @@
+﻿//Программа, которая на вход принимает число(N), а на выходе показывает
+//все четные числа от 1 до N.
+
+Console.WriteLine("Введите число: n");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for(int i=1; i<n; i++)
+{
+    if(i%2==0)
+    {
+        Console.WriteLine(i+"");
+    }
+}
