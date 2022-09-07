ca6e763c4f8819b2fc70e4dc43a2dc0364d7be0b
RRN <nigercanis0@gmail.com>
2022-09-07 22:50:59 +0300

    Init

---
+++ test_№19/Program.cs
@@ -0,0 +1,33 @@
+﻿//Программа, которая принимает на вход
+//пятизначное число и проверяет, является ли оно палиндромом.
+
+string IsPolindrom()
+{
+    Console.WriteLine("Введите число");
+    int num = Convert.ToInt32(Console.ReadLine());
+    int oldnum = num;
+    int revers = 0;
+    while (num > 0)
+    {
+        int dig = num % 10;
+        revers = revers * 10 + dig;
+        num = num / 10;
+    }
+    if (oldnum == revers)
+    {
+        return $"Число является палиндромом";
+    }
+    else
+    {
+        return $"Число не является палиндромом";
+    }
+}
+try
+{
+    Console.WriteLine(IsPolindrom());
+}
+catch
+{
+    Console.WriteLine("Необходимо ввести именно число");
+    //throw;
+}
