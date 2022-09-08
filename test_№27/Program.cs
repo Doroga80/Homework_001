eedfdcf80fd15c27e5fab2158a637630fcb1f771
RRN <rusdorog@yandex.by>
2022-09-08 21:43:47 +0300

    Init

---
+++ test_№27/Program.cs
@@ -0,0 +1,23 @@
+﻿//Программа, которая принимает на вход число и выдает сумму цифр
+//числа.
+
+string SumDig()
+{
+    Console.WriteLine("Введите число ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int sum = 0;
+    while(n > 0)
+    {
+        sum = sum + n % 10;
+        n = n / 10;
+    }
+    return $"Сумма цифр равна {sum}";
+}
+try
+{
+    Console.WriteLine(SumDig());
+}
+catch
+{
+    Console.WriteLine("Необходимо вводить именно число");
+}
