6c551d01972f363475ceb1512dae9be2dd461b4e
RRN <rusdorog@yandex.by>
2022-09-09 19:10:02 +0300

    Init

---
+++ test_№29/Program.cs
@@ -0,0 +1,16 @@
+﻿//Программа, которая задает массив из 8 элементов
+//и выводит их на зкран.
+void PrintArray()
+{
+    int[] numbers = new int[8];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(0, 100 );
+    
+        Console.Write($"{numbers[i]}, ");    
+            
+    }
+    // Console.WriteLine();
+}
+
+PrintArray();
