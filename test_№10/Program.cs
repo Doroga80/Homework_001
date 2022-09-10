1b45e59e904a5820ca8a48aab48118d7647b3b9e
RRN <rusdorog@yandex.by>
2022-09-10 08:34:59 +0300

    Init

---
+++ test_№10/Program.cs
@@ -0,0 +1,13 @@
+﻿//Программа, которая принимает на вход трехзначное число
+//или сооющает, что третей цыфры нет.
+
+void SecondDigit()
+{
+    Console.WriteLine("Введите трехзначное число:");
+    int x = Convert.ToInt32(Console.ReadLine());
+    int x1 = x / 10;
+    int y = x1 % 10;
+    Console.WriteLine("Второй знак в числе равен:" + y);
+}
+
+SecondDigit();
