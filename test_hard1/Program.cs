b9d95e4134e46dedeedf37b41bbddcfe95ae6d8a
RRN <rusdorog@yandex.by>
2022-09-10 09:39:37 +0300

    Init

---
+++ test_hard1/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите число: ");
+string n = Console.ReadLine();
+char[] num = n.ToCharArray();
+int lngth = n.Length;
+for (int i = n.Length - 1; i <= 0; i--)
+    Console.WriteLine($"{num[i]}");
+//     {
+//         if (num[i] == num[lngth-1])
+//         {
+//             polindrom = true;
+//             break;
+//         }
+//     }
+//     if (polindrom) return $"Число {n} является полиндромом";
+//     else return $"Число {n} не является полиндромом";
+
+// try
+// {
+//     Console.WriteLine(Polindrom());
+// }
+// catch
+// {
+//     Console.WriteLine("Необходимо ввести именно числа");
+// }
+
