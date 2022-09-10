f56da866e0fa55235b4bd44b27080462502da0b5
RRN <rusdorog@yandex.by>
2022-09-10 09:28:49 +0300

    Init

---
+++ test_№36/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задайте одномерный маччив, запалненный случайными числами. Найдите
+// сумму элементов, стоящих на нечетных позициях.
+
+int [] PrintArray()
+{
+Console.WriteLine("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] num = new int[n];
+for (int i = 0; i < num.Length; i++)
+{
+    num[i] = new Random().Next(-100, 101);
+    if (i < (num.Length - 1)) Console.Write($"{num[i]}, ");
+    else Console.WriteLine($"{num[i]} ");
+}
+return num;
+}
+string SumOddElements(int [] array)
+{
+int sum = 0;
+for (int j = 0; j < array.Length; j++)
+{
+    if (j % 2 == 1)
+    {
+        sum = sum + array[j];
+    }
+}
+return $"Сумма нечетных элементов = {sum}";
+}
+try
+{
+    int [] mas = PrintArray();
+    System.Console.WriteLine(SumOddElements(mas));
+}
+catch
+{
+    System.Console.WriteLine("error");
+}
