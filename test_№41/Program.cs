b139bdfc4155df836a5cafd35281d71d67495d44
RRN <rusdorog@yandex.by>
2022-09-11 22:28:37 +0300

    Init

---
+++ test_№41/Program.cs
@@ -0,0 +1,27 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитать сколько
+//чисел больще 0 ввел пользователь.
+
+string ElementsGreatZero()
+{
+Console.WriteLine("Введите элементы через пробел: ");
+int[] num = Array.ConvertAll(Console.ReadLine().Split(' '),int.Parse);
+uint count = 0;
+for (int i = 0; i < num.Length; i++)
+{
+    if (num[i] > 0)
+    {
+        count++;
+    }
+}
+return $"Количество элементов больше нуля равно {count}";
+}
+
+try
+{
+    Console.WriteLine(ElementsGreatZero());
+}
+catch
+{
+    Console.WriteLine(" error");
+}
+
