54c81f5e6310ae08ca9e04c2e4288fbc3daa2ae7
RRN <rusdorog@yandex.by>
2022-09-22 20:59:18 +0300

    Init

---
+++ test_№47/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Write("Введите количество строк двумерного массива:");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов двумерного массива:");
+int column = Convert.ToInt32(Console.ReadLine());
+double[,] num = new double[rows, column];
+void PrintArray(double[,] mas)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            Console.Write($"{mas[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(double[,] mas)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            mas[i, j] = Convert.ToDouble(new Random().Next(-100, 100) / 10.0);
+        }
+    }
+}
+try
+{
+    FillArray(num);
+    Console.WriteLine();
+    PrintArray(num);
+}
+catch
+{
+    Console.WriteLine("error");
+}
---
+++ test_№50/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.Write("Введите количество строк двумерного массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов двумерного массива: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] num = new int[rows, column];
+
+int[,] PrintArray(int rows, int column)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            num[i, j] = Convert.ToInt32(new Random().Next(0, 100));
+            Console.Write($"{num[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+    return num;
+}
+string SearchIndex(int[,] array)
+{
+    Console.Write("Введите строку искомого числа: ");
+    int y = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите столбец искомого числа: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    string result;
+    if (y > rows | x > column)
+        result = "Tакого числа нет";
+    else
+    {
+        result = $"Число с индексом[{y},{x}] = {num[y, x]}";
+    }
+    return result;
+}
+try
+{
+    int[,] mas = PrintArray(rows,column);
+    Console.WriteLine();
+    Console.WriteLine(SearchIndex(mas));
+}
+catch
+{
+    Console.WriteLine("error");
+}
