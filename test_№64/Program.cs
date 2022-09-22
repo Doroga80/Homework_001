a55ef61ff98144f01bbc565535c29693a3966bc3
RRN <rusdorog@yandex.by>
2022-09-22 21:35:07 +0300

    Init

---
+++ test_№64/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+static void Foo(int m, int n)
+{
+    if (m > n)
+    {
+        Console.Write("{0} ", m);
+        Foo(m - 1, n);
+    }
+    else if (m < n)
+    {
+        Console.Write("{0} ", m);
+        Foo(m + 1, n);
+    }
+    
+}
+try
+{
+    Foo(m, n);
+    Console.WriteLine();
+}
+catch
+{
+    Console.WriteLine("error");
+}
+
+
